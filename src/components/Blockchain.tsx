428275b10e4ec8def5e8ed178ae126f0d28ceafb
Alex Jewell <alex@alexjewell.com>
2021-01-30 23:36:25 -0600

    Add image

--- src/components/Blockchain.tsx
+++ src/components/Blockchain.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Box, Heading } from 'grommet';
+import { Box, Heading, Image } from 'grommet';
 import RelativeGrid from './RelativeGrid';
 import { useResponsive } from '../utils/useResponsive';
-import Logo from './Logo';
+import LogoImage from '../assets/logo.png';
+import BlockchainImage from '../assets/blockchain.png';
 
 const Blockchain: React.FC<{ setBlockchain: any; }> = (props) => {
   const { setBlockchain } = props;
@@ -13,6 +14,19 @@ const Blockchain: React.FC<{ setBlockchain: any; }> = (props) => {
     font-size: ${isMobile ? '50px' : isTablet ? '125px' : '250px'};
     line-height: ${isMobile ? '65px' : isTablet ? '88px' : '175px'};
     margin: 0;
+    white-space: nowrap; 
+
+    img {
+      display: inline;
+      height: 67px;
+      width: 67px;
+    }
+  `;
+
+  const BlockchainFigure = styled(Image)`
+    display: block;
+    margin-bottom: 50px;
+    width: 100%;
   `;
 
   return (
@@ -25,11 +39,15 @@ const Blockchain: React.FC<{ setBlockchain: any; }> = (props) => {
       fill="vertical"
       gap="none"
     >
-      <Box>
+      <Box pad="large">
         <BlockchainTitle a11yTitle="Blockchain" id="blockchain" level="2" size="large">
           block{!isMobile && <br />}chain
+          <Image a11yTitle="Alex Jewell Logo" alt="Alex Jewell Logo" src={LogoImage} />
         </BlockchainTitle>
         <a href="#" onClick={(e) => { e.preventDefault(); setBlockchain(false); }}>Close</a>
+      </Box>
+      <Box>
+        <BlockchainFigure alt="" src={BlockchainImage} />
       </Box>
     </RelativeGrid>
   );
